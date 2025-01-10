d4b0348ec18865cbc58cac50eecfacd6789b8a19
Tony Kim <tonyhansol.kim@mail.utoronto.ca>
2025-01-09 18:32:40 -0800

    pwm driver code skeleton

--- firmware/STM32/fNIRS/Core/Inc/pwm_driver.h
+++ firmware/STM32/fNIRS/Core/Inc/pwm_driver.h
@@ -2,6 +2,47 @@
 #ifndef INC_PWM_DRIVER_H_
 #define INC_PWM_DRIVER_H_
 
+/* INCLUDES */
+#include "main.h"
+
+/* DEFINES */
+
+/* DATA STRUCTURES */
+typedef enum
+{
+    PWM_CHANNEL0 = 0U, 
+    PWM_CHANNEL1,
+    PWM_CHANNEL2,
+    PWM_CHANNEL3,
+    PWM_CHANNEL4,
+    PWM_CHANNEL5,
+    PWM_CHANNEL6,
+    PWM_CHANNEL7,
+    PWM_CHANNEL8,
+    PWM_CHANNEL9,
+    PWM_CHANNEL10,
+    PWM_CHANNEL11,
+    PWM_CHANNEL12,
+    PWM_CHANNEL13,
+    PWM_CHANNEL14,
+    PWM_CHANNEL15,
+
+    NUM_OF_PWM_CHANNELS,
+} pwm_channel_E;
+
+typedef struct
+{
+    const uint8_t device_address;
+    I2C_HandleTypeDef *i2c_handler;
+} pwm_driver_handler_S;
+
+
+/* FUNCTION DECLARATIONS */
+void pwm_driver_config(pwm_driver_handler_S* handler);
+void pwm_driver_update_frequency(pwm_driver_handler_S* handler);
+// TODO: add phase control to these functions
+void pwm_driver_update_individual_duty_cycle(pwm_driver_handler_S* handler, pwm_channel_E channel, float32_t duty_cycle);
+void pwm_driver_update_all_duty_cycles(pwm_driver_handler_S* handler, float32_t duty_cycle);
 
 
 #endif /* INC_PWM_DRIVER_H_ */
--- firmware/STM32/fNIRS/Core/Src/pwm_driver.c
+++ firmware/STM32/fNIRS/Core/Src/pwm_driver.c
@@ -2,7 +2,49 @@
 /* INCLUDES */
 #include "pwm_driver.h"
 
-
 /* DEFINES */
 
+// Configuration registers
+#define MODE1_ADDR      (0x00)
+#define MODE2_ADDR      (0x01)
+#define SUBADR1_ADDR    (0x02)
+#define SUBADR2_ADDR    (0x03)
+#define SUBADR3_ADDR    (0x04)
+#define ALLCALLADR_ADDR (0x05)
 
+// PWM control registers
+#define LED_ON_L_BASEADDR (0x06)
+#define LED_ON_H_BASEADDR (0x07)
+#define LED_OFF_L_BASEADDR (0x08)
+#define LED_OFF_H_BASEADDR (0x09)
+#define LED_ADDR(addr, offset) (uint8_t)((addr) + (4U)*(offset))
+
+#define ALL_LED_ON_L_ADDR (0xFA)
+#define ALL_LED_ON_H_ADDR (0xFB)
+#define ALL_LED_OFF_L_ADDR (0xFC)
+#define ALL_LED_OFF_H_ADDR (0xFD)
+
+// PWM frequency control register
+#define PRE_SCALE_ADDR  (0xFE)
+
+/* FUNCTION DEFINITIONS */
+
+void pwm_driver_config(pwm_driver_handler_S* handler)
+{
+
+}
+
+void pwm_driver_update_frequency(pwm_driver_handler_S* handler)
+{
+
+}
+
+void pwm_driver_update_individual_duty_cycle(pwm_driver_handler_S* handler, pwm_channel_E channel, float32_t duty_cycle)
+{
+
+}
+
+void pwm_driver_update_all_duty_cycles(pwm_driver_handler_S* handler, float32_t duty_cycle)
+{
+    
+}
