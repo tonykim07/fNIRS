14a9afbee52eb6f520fc1fa04968bbae87eaae9d
Tony Kim <tonyhansol.kim@mail.utoronto.ca>
2025-02-27 21:15:03 -0500

    enum for sensor tx buffer

--- firmware/STM32/fNIRS/Core/Inc/serial_interface.h
+++ firmware/STM32/fNIRS/Core/Inc/serial_interface.h
@@ -23,6 +23,19 @@ typedef enum
 
 } usb_rx_buffer_index_E;
 
+typedef enum
+{
+    SENSOR_CHANNEL_1_H = 0U, // Upper bits for sensor mux1 reading
+    SENSOR_CHANNEL_1_L,      // Lower bits for sensor mux1 reading
+    SENSOR_CHANNEL_2_H,      // Upper bits for sensor mux2 reading
+    SENSOR_CHANNEL_2_L,      // Lower bits for sensor mux2 reading
+    SENSOR_CHANNEL_3_H,      // Upper bits for sensor mux3 reading
+    SENSOR_CHANNEL_3_L,      // Lower bits for sensor mux3 reading
+    EMITTER_STATUS,          // Emitter status: 0 -> off, 1 -> 660NM, 2 -> 940NM
+
+    NUM_OF_BYTES_PER_SENSOR_MODULE,
+} usb_tx_sensor_buffer_index_E;
+
 typedef struct
 {
     // User emitter control variables
@@ -44,5 +57,6 @@ emitter_control_state_E serial_interface_rx_get_emitter_control_state(void);
 uint16_t serial_interface_rx_get_user_emitter_controls(void);
 bool serial_interface_rx_get_user_mux_control_override_enable(void);
 mux_input_channel_E serial_interface_rx_get_user_mux_control_state(void);
+void serial_interface_tx_send_sensor_data(void);
 
 #endif /* INC_SERIAL_INTERFACE_H_ */
