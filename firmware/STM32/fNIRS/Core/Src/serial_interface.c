1565d7301577f6bf91776e18e58ffd8a24c0994a
Tony Kim <tonyhansol.kim@mail.utoronto.ca>
2025-03-02 21:32:31 -0500

    sensor data to serial port implementation

--- firmware/STM32/fNIRS/Core/Src/serial_interface.c
+++ firmware/STM32/fNIRS/Core/Src/serial_interface.c
@@ -2,8 +2,14 @@
 /* INCLUDES */
 #include "serial_interface.h"
 #include "sensing.h"
+#include "emitter_control.h"
+#include "usbd_cdc_if.h"
 
 /* DEFINES */
+#define SENSOR_MODULE_TO_940NM_PWM_CHANNEL(module)  ((module * 2U) + (1U))
+#define SENSOR_MODULE_TO_660NM_PWM_CHANNEL(module)  ((module * 2U))
+
+#define TX_BUFFER_INDEX(sensor_module, byte, bytes_per_sensor) ((sensor_module * bytes_per_sensor) + (byte))
 
 /* DATA STRUCTURES */
 
@@ -52,5 +58,25 @@ mux_input_channel_E serial_interface_rx_get_user_mux_control_state(void)
 
 void serial_interface_tx_send_sensor_data(void)
 {
+    uint8_t tx_buffer[NUM_OF_SENSOR_MODULES * NUM_OF_BYTES_PER_SENSOR_MODULE];
 
+    for (sensor_module_E module = (sensor_module_E)0U; module < NUM_OF_SENSOR_MODULES; module++)
+    {
+        uint16_t sensor_data_channel_one = sensing_get_sensor_calibrated_value(module, MUX_INPUT_CHANNEL_ONE);
+        uint16_t sensor_data_channel_two = sensing_get_sensor_calibrated_value(module, MUX_INPUT_CHANNEL_TWO);
+        uint16_t sensor_data_channel_three = sensing_get_sensor_calibrated_value(module, MUX_INPUT_CHANNEL_THREE);
+        bool emitter_940nm_is_on = emitter_control_is_emitter_active(SENSOR_MODULE_TO_940NM_PWM_CHANNEL(module));
+        bool emitter_660nm_is_on = emitter_control_is_emitter_active(SENSOR_MODULE_TO_660NM_PWM_CHANNEL(module));
+        
+        tx_buffer[TX_BUFFER_INDEX(module, PACKET_IDENTIFIER, NUM_OF_BYTES_PER_SENSOR_MODULE)] = (0xf0 | (uint8_t)module);
+        tx_buffer[TX_BUFFER_INDEX(module, SENSOR_CHANNEL_1_H, NUM_OF_BYTES_PER_SENSOR_MODULE)] = (sensor_data_channel_one >> 8U) & 0xff;
+        tx_buffer[TX_BUFFER_INDEX(module, SENSOR_CHANNEL_1_L, NUM_OF_BYTES_PER_SENSOR_MODULE)] = (sensor_data_channel_one) & 0xff;
+        tx_buffer[TX_BUFFER_INDEX(module, SENSOR_CHANNEL_2_H, NUM_OF_BYTES_PER_SENSOR_MODULE)] = (sensor_data_channel_two >> 8U) & 0xff;
+        tx_buffer[TX_BUFFER_INDEX(module, SENSOR_CHANNEL_2_L, NUM_OF_BYTES_PER_SENSOR_MODULE)] = (sensor_data_channel_two) & 0xff;
+        tx_buffer[TX_BUFFER_INDEX(module, SENSOR_CHANNEL_3_H, NUM_OF_BYTES_PER_SENSOR_MODULE)] = (sensor_data_channel_three) & 0xff;
+        tx_buffer[TX_BUFFER_INDEX(module, SENSOR_CHANNEL_3_L, NUM_OF_BYTES_PER_SENSOR_MODULE)] = (sensor_data_channel_three) & 0xff;
+        tx_buffer[TX_BUFFER_INDEX(module, EMITTER_STATUS, NUM_OF_BYTES_PER_SENSOR_MODULE)] = ((uint8_t)emitter_940nm_is_on << 1) | ((uint8_t)emitter_660nm_is_on);
+    }
+
+    CDC_Transmit_FS(tx_buffer, sizeof(tx_buffer));   
 }
